f61bd7f2d214f46e5eca0ead3c2f7a679040293e
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-08-03 17:25:19 +0200

    Refactored err handling to purely use the global handler and custom error types

--- src/api/chirp.ts
+++ src/api/chirp.ts
@@ -1,40 +1,31 @@
 import type { Request, Response } from "express";
 import { config } from "../config.js";
+import { BadRequestError } from "../error.js";
 
 export function handlerValidateChirp(req: Request, res: Response) {
   type parameters = {
     body: string;
   };
 
-  try {
-    res.header("Content-Type", "application/json");
+  res.header("Content-Type", "application/json");
 
-    const params: parameters = req.body;
-    if (params.body === undefined) {
-      throw new Error("Something went wrong");
-    }
-
-    if (params.body.length > 140) {
-      throw new Error("Chirp is too long");
-    }
-
-    let clean = "";
-    const splitMsg = params.body.split(" ");
-    for (let pos in splitMsg) {
-      if (config.badWords.includes(splitMsg[pos].toLowerCase())) {
-        splitMsg[pos] = "****";
-      }
-    }
-
-    res.send(JSON.stringify({cleanedBody: splitMsg.join(" ")}));
-    res.end();
-  } catch (error) {
-    let msg = "Something went wrong";
-    if (error instanceof Error) msg = error.message;
-
-    if (msg === "Chirp is too long") throw new Error("Chirp is too long");
-
-    res.status(400).send(JSON.stringify({error: msg}));
-    res.end();
+  const params: parameters = req.body;
+  if (params.body === undefined) {
+    throw new Error("Something went wrong");
   }
+
+  if (params.body.length > 140) {
+    throw new BadRequestError("Chirp is too long. Max length is 140");
+  }
+
+  let clean = "";
+  const splitMsg = params.body.split(" ");
+  for (let pos in splitMsg) {
+    if (config.badWords.includes(splitMsg[pos].toLowerCase())) {
+      splitMsg[pos] = "****";
+    }
+  }
+
+  res.send(JSON.stringify({cleanedBody: splitMsg.join(" ")}));
+  res.end();
 }
--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,8 +1,33 @@
 import type { Request, Response, NextFunction } from "express";
+import { BadRequestError, UnauthorizedError, ForbiddenError, NotFoundError } from "../error.js";
 
 export function middlewareError(err: Error, req: Request, res: Response, next: NextFunction) {
+  if (err instanceof BadRequestError) {
+    res.status(400).json({error: err.message});
+    res.end();
+    return;
+  }
+
+  if (err instanceof UnauthorizedError) {
+    res.status(401).json({error: err.message});
+    res.end();
+    return;
+  }
+
+  if (err instanceof ForbiddenError) {
+    res.status(403).json({error: err.message});
+    res.end();
+    return;
+  }
+
+  if (err instanceof NotFoundError) {
+    res.status(404).json({error: err.message});
+    res.end();
+    return;
+  }
+
   console.log(err.message);
   res.status(500).json({
-    error: "Something went wrong on our end",
+    error: "Internal Server Error",
   });
 }
