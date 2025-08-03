1a8fcfc521d7a15a9e330a21ad75691a1d909386
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-08-03 15:40:42 +0200

    Metrics inc on app hits

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { middlewareLogResponses } from "./middleware/log.js";
+import { middlewareMetricsInc } from "./middleware/metrics.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
-app.use("/app", express.static("./src/app"));
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
