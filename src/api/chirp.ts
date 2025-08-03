40509e5a094ca0f8cd921c86eb40b3a60f33a68b
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-08-03 16:36:15 +0200

    Reworked to use express convenient json parsing

--- src/api/chirp.ts
+++ src/api/chirp.ts
@@ -1,32 +1,28 @@
 import type { Request, Response } from "express";
 
 export function handlerValidateChirp(req: Request, res: Response) {
-  let body = "";
+  type parameters = {
+    body: string;
+  };
 
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
+  try {
+    res.header("Content-Type", "application/json");
 
-  req.on("end", () => {
-    try {
-      res.header("Content-Type", "application/json");
-
-      const parsedBody = JSON.parse(body);
-      if (!parsedBody.hasOwnProperty("body")) {
-        throw new Error("Something went wrong");
-      }
-
-      if (parsedBody.body.length > 140) {
-        throw new Error("Chirp is too long");
-      }
-
-      res.send(JSON.stringify({valid: true}));
-      res.end();
-    } catch (error) {
-      let msg = "Something went wrong";
-      if (error instanceof Error) msg = error.message;
-      res.status(400).send(JSON.stringify({error: msg}));
-      res.end();
+    const params: parameters = req.body;
+    if (params.body === undefined) {
+      throw new Error("Something went wrong");
     }
-  });
+
+    if (params.body.length > 140) {
+      throw new Error("Chirp is too long");
+    }
+
+    res.send(JSON.stringify({valid: true}));
+    res.end();
+  } catch (error) {
+    let msg = "Something went wrong";
+    if (error instanceof Error) msg = error.message;
+      res.status(400).send(JSON.stringify({error: msg}));
+    res.end();
+  }
 }
