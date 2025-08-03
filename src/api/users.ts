793df5258c72bdfa2cb387641e4e7348cd36f892
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-08-03 21:09:33 +0200

    Updated type to match expected and hide password

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 
 import { createUser, getUser } from "../db/queries/users.js";
-import { NewUser, User } from "../db/schema.js";
+import { NewUser } from "../db/schema.js";
 import { BadRequestError, UnauthorizedError } from "../error.js";
 import { hashPassword, checkPasswordHash } from "../auth.js";
+
+export type UserResponse = Omit<NewUser, "hashed_password">
 
 export async function handlerCreateUser(req: Request, res: Response) {
   type parameters = {
@@ -18,8 +20,13 @@ export async function handlerCreateUser(req: Request, res: Response) {
 
   const hashed_password = await hashPassword(params.password);
 
-  const newUser = await createUser({email: params.email, hashed_password: hashed_password}) as Omit<NewUser, "hashed_password">;
-  res.status(201).json(newUser);
+  const user = await createUser({email: params.email, hashed_password: hashed_password});
+  res.status(201).json({
+    id: user.id,
+    email: user.email,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  } satisfies UserResponse);
   res.end();
 }
 
@@ -44,7 +51,11 @@ export async function handlerLogin(req: Request, res: Response) {
     throw new UnauthorizedError("Incorrect email or password");
   }
 
-  const retUser = user as Omit<User, "hashed_password">;
-  res.status(200).json(retUser);
+  res.status(200).json({
+    id: user.id,
+    email: user.email,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  } satisfies UserResponse);
   res.end();
 }
