123cef2840fff082bc950fb69c8df91679fc42ce
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-08-03 15:40:52 +0200

    Register metrics endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { handlerMetrics, handlerReset } from "./api/metrics.js";
 import { middlewareLogResponses } from "./middleware/log.js";
 import { middlewareMetricsInc } from "./middleware/metrics.js";
 
@@ -9,6 +10,8 @@ const PORT = 8080;
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerMetrics);
+app.get("/reset", handlerReset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
