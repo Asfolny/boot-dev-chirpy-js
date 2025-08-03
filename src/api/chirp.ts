7d2da9d5cbc885d2f668b710995be5e6921541f6
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-08-03 18:41:58 +0200

    Resturctured chirps endpoints to allow creating chirps in the db

--- src/api/chirp.ts
+++ src/api/chirp.ts
@@ -2,30 +2,34 @@ import type { Request, Response } from "express";
 import { config } from "../config.js";
 import { BadRequestError } from "../error.js";
 
-export function handlerValidateChirp(req: Request, res: Response) {
-  type parameters = {
-    body: string;
-  };
-
-  res.header("Content-Type", "application/json");
-
-  const params: parameters = req.body;
-  if (params.body === undefined) {
-    throw new Error("Something went wrong");
-  }
-
-  if (params.body.length > 140) {
+function validateChirp(msg: string) {
+  if (msg.length > 140) {
     throw new BadRequestError("Chirp is too long. Max length is 140");
   }
 
   let clean = "";
-  const splitMsg = params.body.split(" ");
+  const splitMsg = msg.split(" ");
   for (let pos in splitMsg) {
     if (config.api.badWords.includes(splitMsg[pos].toLowerCase())) {
       splitMsg[pos] = "****";
     }
   }
 
-  res.send(JSON.stringify({cleanedBody: splitMsg.join(" ")}));
+  return splitMsg.join(" ");
+}
+
+export async function handlerCreateChirps(req: Request, res: Response) {
+  type parameters = {
+    body: string;
+  };
+
+  const params: parameters = req.body;
+  if (params.body === undefined) {
+    throw new Error("Something went wrong");
+  }
+
+  const validatedMst = validateChirp(params.body);
+
+  res.json({validatedMsg});
   res.end();
 }
