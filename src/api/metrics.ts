06cf7f43f656abc1144242b12a53275cbe1ae583
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-08-03 15:47:03 +0200

    Changed metrics to be a html template

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -3,7 +3,15 @@ import type { Request, Response } from "express";
 import { config } from "../config.js";
 
 export function handlerMetrics(_req: Request, res: Response) {
-  res.send(`Hits: ${config.fileserverHits}`);
+  res.set({
+    'Content-Type': 'text/html; charset=utf-8',
+  });
+  res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+  </body>
+</html>`);
   res.end();
 }
 
